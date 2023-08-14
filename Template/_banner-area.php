1169048c67fc6816cd8e164444420a641fcbd9a0
SupunThilanka <e19349@eng.pdn.ac.lk>
2023-08-15 01:17:53 +0530

    Update _banner-area.php

--- Template/_banner-area.php
+++ Template/_banner-area.php
@@ -2,9 +2,6 @@
 <section id="banner-area">
     <div class="owl-carousel owl-theme">
         <div class="item">
-            <img src="./assets/Banner1.jpg" alt="Banner1">
-        </div>
-        <div class="item">
             <img src="./assets/Banner2.jpg" alt="Banner2">
         </div>
         <div class="item">
@@ -12,6 +9,9 @@
         </div>
         <div class="item">
             <img src="./assets\Banner4.jpg" alt="Banner4">
+        </div>
+        <div class="item">
+            <img src="./assets/Banner1.jpg" alt="Banner1">
         </div>
     </div>
 </section>
